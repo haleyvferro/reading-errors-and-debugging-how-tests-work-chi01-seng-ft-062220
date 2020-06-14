7e17b38658dc56c1fe0791e11e60afecd3c9b660
Haley Ferro <haleyvferro@gmail.com>
2020-06-14 17:59:52 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,19 +1,16 @@
-def addition(first_number, second_number)
+
 sum = first_number + second_number
 sum
 end
 
-def subtraction(first_number, second_number)
 sum = first_number - second_number
 sum
 end
 
-def division(first_number, second_number)
 sum = first_number / second_number
 sum
 end
 
-def multiplication(first_number, second_number)
 sum = first_number * second_number
 sum
 end
