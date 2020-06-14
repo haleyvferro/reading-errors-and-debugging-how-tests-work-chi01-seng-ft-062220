564cf3852763ab92ac50885fc207d90c624dadd2
Haley Ferro <haleyvferro@gmail.com>
2020-06-14 18:00:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,15 +4,3 @@ second_number = 2
 sum = first_number + second_number
 sum
 end
-
-sum = first_number - second_number
-sum
-end
-
-sum = first_number / second_number
-sum
-end
-
-sum = first_number * second_number
-sum
-end
